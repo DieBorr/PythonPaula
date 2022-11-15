c85b98f0f1206bbab36422b1a430036e41173923
DieBorr <dieborr3@gmail.com>
2022-11-15 23:12:44 +0100

    Second aproach

--- busca_imagenes.py
+++ busca_imagenes.py
@@ -6,11 +6,14 @@ files_names = os.listdir(input_images_path)
 
 
 for file_name in files_names:
-    print(file_name)
-    image_path = input_images_path + "/" + file_name
-    print(image_path)
-    image = cv2.imread(image_path)
+    #print(file_name)
+    if '.jpg' in file_name:
+        image_path = input_images_path + "/" + file_name
+        image = cv2.imread(image_path)
 
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
-cv2.destroyAlllWindows()
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+    else:
+        print(file_name)
+        continue
+
